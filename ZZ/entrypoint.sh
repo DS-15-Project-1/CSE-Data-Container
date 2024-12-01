8dbf6e24482984d03fead66659c0c40b79ddcbbb
RSBalchII <robertbalchii@gmail.com>
2024-12-01 12:20:57 -0700

    main: updated dockerfiles

---
+++ ZZ/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Activate the Conda environment
+source /opt/conda/etc/profile.d/conda.sh
+conda activate myenv
+
+# Run the Python script
+exec python /app/mseed_parquet_ZZ.py
