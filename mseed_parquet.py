e3131e395f86932c1a740a8e9e488e10194cb802
RSBalchII <robertbalchii@gmail.com>
2024-12-03 09:20:28 -0700

    rob-branch-revert: New ZZmseed conversion format

--- mseed_parquet.py
+++ mseed_parquet.py
@@ -85,9 +85,9 @@ def process_directory(directory_path, input_dir, output_dir):
     
     for rel_path, file in tqdm(dir_files, desc=f"Processing directory {directory_path}"):
         input_file = os.path.join(input_dir, rel_path, file)
+        
         # Create a unique output file name for each input file
         output_file = os.path.join(output_dir, rel_path, f"{os.path.splitext(file)[0]}_{os.path.basename(rel_path)}.parquet")
-        
         os.makedirs(os.path.dirname(output_file), exist_ok=True)
         
         success = convert_file_to_parquet(input_file, output_file)
